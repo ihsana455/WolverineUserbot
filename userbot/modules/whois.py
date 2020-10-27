f904ed2e164130bb95fe0cd6afd25c52f93cc0c5
shadowclub <73276527+shadowclub@users.noreply.github.com>
2020-10-27 15:52:02 +0530

    Update whois.py

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -22,7 +22,7 @@ from userbot.events import register
 async def who(event):
 
     await event.edit(
-        "`Sit tight while I steal some data from *Global Network Zone*...`"
+        "`Sit tight while I steal some data from *Global Network📡 Zone*...📥`"
     )
 
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
@@ -33,7 +33,7 @@ async def who(event):
     try:
         photo, caption = await fetch_info(replied_user, event)
     except AttributeError:
-        return event.edit("`Could not fetch info of that user.`")
+        return event.edit("`Could not fetch info of that user 😕`")
 
     message_id_to_reply = event.message.reply_to_msg_id
 
